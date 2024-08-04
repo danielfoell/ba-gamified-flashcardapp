679840f22c39d568196719b7ee6c8e1c7555e4e7
mamasgoodboy <daniel.foell@gmx.de>
2024-08-05 01:16:58 +0200

    .

--- Flashcards/LearnFlashcardsView.gd
+++ Flashcards/LearnFlashcardsView.gd
@@ -6,13 +6,30 @@ extends Panel
 @onready var Progress = $Header/MarginContainer/HBoxContainer/Progress
 
 var currentFlashcard: FlashcardData
+var currentDeck: DeckData
 
 func _ready():
-	var currentDeck: DeckData = StorageService.currentDeck
+	currentDeck = StorageService.currentDeck
 	currentFlashcard = currentDeck.learningFlashcards.front()
 	CardsCount.text = "%s/%s" % [1, currentDeck.learningFlashcards.size()]
 	Progress.set_max(currentDeck.learningFlashcards.size())
 	Progress.set_value(0)
 
-func _process(delta):
-	pass
+func _On_BtnBad_Pressed():
+	pass # Replace with function body.
+
+func _On_BtnGood_Pressed():
+	SetNextFlashcard()
+
+
+func GetNextFlashcard():
+	return currentDeck.learningFlashcards[currentDeck.learningFlashcards.find(currentFlashcard) + 1]
+
+func SetNextFlashcard():
+	currentFlashcard = GetNextFlashcard()
+	var tween = get_tree().create_tween()
+	#Progress.set_value(Progress.get_value() + 1)
+	tween.tween_property(Progress, "value", 2, 1).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	#tween.play()
+	
+	CardsCount.text = "%s/%s" % [currentDeck.learningFlashcards.find(currentFlashcard) + 1, currentDeck.learningFlashcards.size()]
