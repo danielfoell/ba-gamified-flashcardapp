06f539bcf287c97ed2eacbdcab51a5c04132d4cc
mamasgoodboy <daniel.foell@gmx.de>
2024-10-13 20:27:33 +0200

    13.10.2024

--- DailyReward.gd
+++ DailyReward.gd
@@ -1,6 +1,28 @@
 extends Control
 
 @onready var Unlock = $Panel/MarginContainer/VBoxContainer/MarginContainer/ScrollContainer/HBoxContainer/DayOne/Unlock/AnimatedSprite2D
+var DailyRewards = {
+	"ONE": {
+		"EXP": 0,
+		"COINS": 15
+	},
+	"TWO": {
+		"EXP": 250,
+		"COINS": 0
+	},
+	"THREE": {
+		"EXP": 400,
+		"COINS": 50
+	},
+	"FOUR": {
+		"EXP": 0,
+		"COINS": 30
+	},
+	"FIVE": {
+		"EXP": 1500,
+		"COINS": 250
+	}
+}
 
 func _ready():
 	Unlock.play("unlock")
